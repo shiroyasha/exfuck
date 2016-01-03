51bcf9dfe8dfe64073775865df87195ffee8c6d8
Igor Šarčević <igor@renderedtext.com>
2016-01-03 19:24:58 +0100

    Simple increment

--- lib/exfuck.ex
+++ lib/exfuck.ex
@@ -1,11 +1,60 @@
 defmodule Exfuck do
 
+  @initial_state %{
+    :tape => [0],
+    :tape_pointer => 0,
+    :code => "",
+    :code_pointer => 0,
+    :output => []
+  }
+
   def run(code) do
-    do_run(code, [0])
+    state = %{ @initial_state | :code => code }
+
+    result_state = do_run(state)
+
+    result_state[:output] |> List.to_string
   end
 
-  defp do_run(code, tape) do
-    "Hello world!\n"
+  defp do_run(state) do
+    if state[:code_pointer] < String.length(state[:code]) do
+      new_state = run_instruction(state, current_instruction(state))
+
+      display_state new_state
+
+      do_run %{new_state | :code_pointer => state[:code_pointer] + 1}
+    else
+      state
+    end
+  end
+
+  defp current_instruction(state) do
+    String.at(state[:code], state[:code_pointer])
+  end
+
+  defp current_value(state) do
+    Enum.at(state[:tape], state[:tape_pointer])
+  end
+
+  defp display_state(state) do
+    IO.puts "tape: #{state[:tape]}"
+    IO.puts "tape_pointer: #{state[:tape_pointer]}"
+    IO.puts "code: #{state[:code]}"
+    IO.puts "code_pointer: #{state[:code_pointer]}"
+    IO.puts "output: #{state[:output]}"
+  end
+
+
+  defp run_instruction(state, ".") do
+    %{state | :output => state[:output] ++ [current_value(state)]}
+  end
+
+  defp run_instruction(state, "+") do
+    new_tape = List.replace_at(state[:tape],
+                               state[:tape_pointer],
+                               current_value(state) + 1)
+
+    %{state | :tape => new_tape}
   end
 
 end
--- test/exfuck_test.exs
+++ test/exfuck_test.exs
@@ -3,42 +3,36 @@ defmodule ExfuckTest do
   doctest Exfuck
 
   test "display current pointer" do
-    code = "."
+    code = "++++++++++."
 
-    assert Exfuck.run(code) == "0"
+    assert Exfuck.run(code) == "\n"
   end
 
-  test "simple increment" do
-    code = "+++."
+  # test "hello world compilation" do
+  #   hello_world = """
+# +++++ +++++             initialize counter (cell #0) to 10
+# [                       use loop to set 70/100/30/10
+  #   > +++++ ++              add  7 to cell #1
+  #   > +++++ +++++           add 10 to cell #2
+  #   > +++                   add  3 to cell #3
+  #   > +                     add  1 to cell #4
+# <<<< -                  decrement counter (cell #0)
+# ]
+# > ++ .                  print 'H'
+# > + .                   print 'e'
+# +++++ ++ .              print 'l'
+# .                       print 'l'
+# +++ .                   print 'o'
+# > ++ .                  print ' '
+# << +++++ +++++ +++++ .  print 'W'
+# > .                     print 'o'
+# +++ .                   print 'r'
+# ----- - .               print 'l'
+# ----- --- .             print 'd'
+# > + .                   print '!'
+# > .                     print '\n'
+  #   """
 
-    assert Exfuck.run(code) == "3"
-  end
-
-  test "hello world compilation" do
-    hello_world = """
-+++++ +++++             initialize counter (cell #0) to 10
-[                       use loop to set 70/100/30/10
-    > +++++ ++              add  7 to cell #1
-    > +++++ +++++           add 10 to cell #2
-    > +++                   add  3 to cell #3
-    > +                     add  1 to cell #4
-<<<< -                  decrement counter (cell #0)
-]
-> ++ .                  print 'H'
-> + .                   print 'e'
-+++++ ++ .              print 'l'
-.                       print 'l'
-+++ .                   print 'o'
-> ++ .                  print ' '
-<< +++++ +++++ +++++ .  print 'W'
-> .                     print 'o'
-+++ .                   print 'r'
------ - .               print 'l'
------ --- .             print 'd'
-> + .                   print '!'
-> .                     print '\n'
-    """
-
-    assert Exfuck.run(hello_world) == "Hello world!\n"
-  end
+  #   assert Exfuck.run(hello_world) == "Hello world!\n"
+  # end
 end
