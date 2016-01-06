1edab1697963ac2c583d97c80ca8b20ec0168815
Igor Šarčević <igor@renderedtext.com>
2016-01-06 22:48:28 +0100

    Update hex package info

--- mix.exs
+++ mix.exs
@@ -7,25 +7,22 @@ defmodule Exfuck.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  def package do
+    [
+      maintainers: ["Igor Šarčević (shiroyasha)"],
+      links: %{"GitHub": "https://github.com/shiroyasha/exfuck"},
+      licenses: ["MIT"]
+    ]
+  end
+
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
